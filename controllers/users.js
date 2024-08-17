40743d654358f8c8d576531fb83581ee31b0bd8d
haiaza <ajames1342@gmail.com>
2024-08-17 10:58:36 -0400

    Added Controllers. Paintings controller TBD

---
+++ controllers/users.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user.js')
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find({}) 
+        res.render('users/index.ejs', { users })
+}   catch (error) {
+        console.error(error)
+        res.redirect('/')
+}
+});
+
+router.get('/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId
+        const user = await User.findById(userId) 
+
+    if (!user) {
+        return res.redirect('/users')
+    }
+    res.render('users/show.ejs', { user, painting })
+    } catch (error) {
+    console.error(error)
+    res.redirect('/users')
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const app = express()
+const morgan = require('morgan')
+const session = require('express-session')
+const authController = require('./controllers/auth.js')
+const paintingController = require('./controllers/paintings.js')
+const usersController = require('./controllers/users.js')
 const MONGO_URI = process.env.MONGO_URI
 
 mongoose.connect(MONGO_URI)
