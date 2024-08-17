a7e20b6ad0bb7f4cb8161faff22079da19ce1393
haiaza <ajames1342@gmail.com>
2024-08-17 17:34:26 -0400

    RevertedCommunity Index and Show pages done. Bug created with vall paintings showing.

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user.js')
+const Painting = require('../models/painting.js')
 
 router.get('/', async (req, res) => {
     try {
@@ -13,6 +14,22 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId
+        const user = await User.findById(userId) 
+        const paintings = await Painting.find(userId.username) 
+
+    if (!user) {
+        return res.redirect('/users')
+    }
+    res.render('users/show.ejs', { user, paintings })
+    } catch (error) {
+    console.error(error)
+    res.redirect('/users')
+    }
+})
+
+router.get('/:userId/', async (req, res) => {
     try {
         const userId = req.params.userId
         const user = await User.findById(userId) 
