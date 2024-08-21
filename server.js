c5b05fe36a54dcc1907ccaa1d9e5fc6e21ace2df
haiaza <ajames1342@gmail.com>
2024-08-20 23:16:18 -0400

    dotenv missing.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
